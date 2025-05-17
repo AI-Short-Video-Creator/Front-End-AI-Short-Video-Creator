c5a3b96886afe5fc381b97e68c7d6c111324a15e
My Truong <truongthitumy2810@gmail.com>
2025-05-17 09:26:29 +0700

    update something

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Create from "./pages/Create";
-import Library from "./pages/Library";
+//import Create from "./pages/Create";
+//import Library from "./pages/Library";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import Auth from "./pages/Auth";
@@ -32,8 +32,8 @@ const App = () => (
             {/* Protected Routes */}
             <Route element={<Auth />}>
               <Route path="/" element={<Index />} />
-              <Route path="/create" element={<Create />} />
-              <Route path="/library" element={<Library />} />
+              {/* <Route path="/create" element={<Create />} />
+              <Route path="/library" element={<Library />} /> */}
               <Route path="/profile" element={<Profile />} />
               <Route path="/settings" element={<Settings />} />
             </Route>
